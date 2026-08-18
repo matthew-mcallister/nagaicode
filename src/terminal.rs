22c9f2677307d37e9954e0c1d08e6678e671fdba
Matthew McAllister <matthew.mcallister.0@gmail.com>
2026-08-18 12:59:52 -0700

    Add dummy terminal for testing

--- src/terminal.rs
+++ src/terminal.rs
@@ -5,7 +5,7 @@ use futures::Stream;
 
 use crate::error::AnyResult;
 
-pub use detail::CrosstermTerminal as DefaultTerminal;
+pub use detail::DefaultTerminal;
 
 /// A wrapper around the terminal API that can be used to simulate a terminal
 /// in tests.
@@ -26,6 +26,7 @@ pub trait Terminal {
     fn disable_raw_mode(&self) -> AnyResult<()>;
 }
 
+#[cfg(not(test))]
 mod detail {
     use std::io::Write;
 
@@ -38,12 +39,12 @@ mod detail {
 
     /// Terminal implementation using crossterm
     #[derive(Debug)]
-    pub struct CrosstermTerminal {
+    pub struct DefaultTerminal {
         stdout: std::io::Stdout,
         events: EventStream,
     }
 
-    impl Default for CrosstermTerminal {
+    impl Default for DefaultTerminal {
         fn default() -> Self {
             Self {
                 stdout: std::io::stdout(),
@@ -52,13 +53,95 @@ mod detail {
         }
     }
 
-    impl CrosstermTerminal {
+    impl DefaultTerminal {
         pub fn new() -> Self {
             Self::default()
         }
     }
 
-    impl Terminal for CrosstermTerminal {
+    impl Terminal for DefaultTerminal {
+        fn stdout(&mut self) -> &mut impl Write {
+            &mut self.stdout
+        }
+
+        fn events(&mut self) -> &mut impl Stream<Item = std::io::Result<Event>> {
+            &mut self.events
+        }
+
+        fn size(&self) -> AnyResult<(u16, u16)> {
+            Ok(size()?)
+        }
+
+        fn enable_raw_mode(&self) -> AnyResult<()> {
+            Ok(enable_raw_mode()?)
+        }
+
+        fn disable_raw_mode(&self) -> AnyResult<()> {
+            Ok(disable_raw_mode()?)
+        }
+    }
+}
+
+#[cfg(test)]
+mod detail {
+    use std::io::Write;
+    use std::task::Poll;
+
+    use crossterm::event::Event;
+    use crossterm::terminal::{disable_raw_mode, enable_raw_mode, size};
+    use futures::{Stream, pin_mut};
+
+    use super::Terminal;
+    use crate::error::AnyResult;
+
+    /// Simulates an async stream by yielding from a vector.
+    #[derive(Clone, Debug)]
+    pub struct VecStream<E>(pub Vec<E>);
+
+    impl<E> Default for VecStream<E> {
+        fn default() -> Self {
+            Self(Default::default())
+        }
+    }
+
+    impl<E> Unpin for VecStream<E> {}
+
+    impl<E> Stream for VecStream<E> {
+        type Item = E;
+
+        fn poll_next(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> Poll<Option<Self::Item>> {
+            let this = self;
+            pin_mut!(this);
+            Poll::Ready(this.0.pop())
+        }
+    }
+
+    /// Dummy terminal implementation
+    #[derive(Debug)]
+    pub struct DefaultTerminal {
+        pub stdout: Vec<u8>,
+        pub events: VecStream<std::io::Result<Event>>,
+    }
+
+    impl Default for DefaultTerminal {
+        fn default() -> Self {
+            Self {
+                stdout: Vec::new(),
+                events: VecStream::default(),
+            }
+        }
+    }
+
+    impl DefaultTerminal {
+        pub fn new() -> Self {
+            Self::default()
+        }
+    }
+
+    impl Terminal for DefaultTerminal {
         fn stdout(&mut self) -> &mut impl Write {
             &mut self.stdout
         }
